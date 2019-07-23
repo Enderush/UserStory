e2bdba9ca9a1d6c5c2865089d844fd19960c3ede
christiandowns04@gmail.com <christiandowns04@gmail.com>
2019-07-23 09:22:16 -0700

    Add project files.

---
+++ UserStory/UserStory.cpp
@@ -0,0 +1,73 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string noun1;
+	string noun2;
+	string noun3;
+	string verb1;
+	string noun4;
+	string noun5;
+	string noun6;
+	string noun7;
+	string verb4;
+	string noun8;
+	string verb5;
+	string number;
+	string adjective1;
+	string noun9;
+
+	cout << "Type a noun. Please make all of your answers single words. Press enter when you are done." << endl;
+	cin >> noun1;
+	cout << "Type a noun." << endl;
+	cin >> noun2;	
+	cout << "Type a noun. Please capitalize this one. :)" << endl;
+	cin >> noun3;
+	cout << "Type a verb." << endl;
+	cin >> verb1;
+	cout << "Type a noun. Not done yet. :)" << endl;
+	cin >> noun4;
+	cout << "Type a noun. Getting closer. :)" << endl;
+	cin >> noun5;
+	cout << "Type a noun. Almost there! :) Also, make this one plural please!" << endl;
+	cin >> noun6;
+	cout << "Type a noun. This is the last one! :D" << endl;
+	cin >> noun9;
+	cout << "Type an adjective. I lied :3" << endl;
+	cin >> verb4;
+	cout << "Type a noun." << endl;
+	cin >> noun7;
+	cout << "Type a noun." << endl;
+	cin >> noun8;
+	cout << "Type a number." << endl;
+	cin >> number;
+	cout << "Type an adjective." << endl;
+	cin >> adjective1;
+	cout << "Type a verb. This is the last one, I promise!:D" << endl;
+		cin >> verb5;
+
+
+
+	
+
+	
+		
+		
+		
+	cout << "Once upon a " << noun1 << ", my " << noun2 << " and I went to " << noun3 << " Theme Park." << endl;
+	cout << "When we " << verb1 << " into the park, the first ride we rode on was " << noun4 << ". That was a fun one!" << endl;
+	cout << "Then we rode on the imfamous " << noun5 << ". It was scary, but I made it out alive!" << endl;
+	cout << "We decided to have a snack , so we sat down to " << verb5 << " a nice hot cup of " << noun7 << "." << endl;
+	cout << "It was " << noun9 << " , so we decided to head home." << endl;
+	cout << "We " << verb4 << " home. On the way home, I saw " << number << " " << adjective1 << " " << noun6 << "." << endl;
+	cout << "When we got home, we relaxed and played fetch with our adorable " << noun8 << "." << endl;
+
+
+
+
+
+	return 0;
+}
